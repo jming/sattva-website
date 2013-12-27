70665398969639088cf1b6d8ca6d8ab2bd4b653c
jming <jming@college.harvard.edu>
2013-12-27 00:15:13 -0500

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+	return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'img'),
+                               'favicon.ico', mimetype='image/png')
